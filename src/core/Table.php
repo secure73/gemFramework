bd4aae83fa97937e31e1525408f14f6e827057f3
secure73 <secure73@gmail.com>
2023-11-09 17:32:33 +0100

    Update Table.php extends PdoQuery

--- src/core/Table.php
+++ src/core/Table.php
@@ -2,14 +2,9 @@
 namespace GemFramework\Core;
 
 use GemFramework\Database\PdoConnManager;
-/**
- * @method int|null insert@uery()
- * @method array|null selectQuery()
- * @method int|false countQuery()
- * @method int|null updateQuery()
- * @method int|null deleteQuery()
- */
- class Table extends PdoConnManager
+use GemLibrary\Database\PdoQuery;
+
+class Table extends PdoQuery
 {
     /**
      * @param string|null $connectionName
@@ -17,106 +12,8 @@ use GemFramework\Database\PdoConnManager;
      */
     public function __construct(?string $connectionName = null)
     {
-        parent::__construct($connectionName);
-    }
-    /**
-     * @param string $insertQuery Sql insert query
-     * @param array<string,mixed> $arrayBindKeyValue
-     *
-     * @return null|int
-     * $query example: 'INSERT INTO users (name,email,password) VALUES (:name,:email,:password)'
-     * arrayBindKeyValue Example [':name' => 'some new name' , ':email' => 'some@me.com , :password =>'si§d8x']
-     * success : return last insertd id
-     * you can call affectedRows() to get how many rows inserted
-     * error: $this->getError();
-     */
-    public function insertQuery(string $insertQuery, array $arrayBindKeyValue = []): int|null
-    {
-        if($this->isConnected()) {
-            if ($this->executeQuery($insertQuery, $arrayBindKeyValue)) {
-                return (int) $this->lastInsertId();
-            }
-        }
-        return null;
-    }
-
-    /**
-     * @param array<mixed> $arrayBindKeyValue
-     *
-     * @return null|array<mixed>
-     *
-     * @$query example: 'SELECT * FROM users WHERE email = :email'
-     * @arrayBindKeyValue Example [':email' => 'some@me.com']
-     */
-    public function selectQuery(string $selectQuery, array $arrayBindKeyValue = []): array|null
-    {
-        if(!$this->isConnected())
-        {
-            return null;
-        }
-         if ($this->executeQuery($selectQuery, $arrayBindKeyValue)) {
-                return $this->fetchAllObjects();
-        }
-        return null;
-    }
-
-    /**
-     * @param array<mixed> $arrayBindKeyValue
-     *
-     * @$query example: 'SELECT count(*) FROM users WHERE name LIKE :name'
-     *
-     * @arrayBindKeyValue Example [':name' => 'someone']
-     */
-    public function countQuery(string $selectCountQuery, array $arrayBindKeyValue = []): int|false
-    {
-        $result = false;
-        if($this->isConnected()) {
-            if ($this->executeQuery($selectCountQuery, $arrayBindKeyValue)) {
-                $result = $this->fetchColumn();
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * @param array<string,mixed> $arrayBindKeyValue
-     *
-     * @return null|int
-     * $query example: 'UPDATE users SET name = :name , isActive = :isActive WHERE id = :id'
-     * arrayBindKeyValue Example [':name' => 'some new name' , ':isActive' => true , :id => 32 ]
-     * in success return positive number affected rows and in error null
-     */
-    public function updateQuery(string $updateQuery, array $arrayBindKeyValue = []): int|null
-    {
-        $result = null;
-        if($this->isConnected()) {
-            if ($this->executeQuery($updateQuery, $arrayBindKeyValue)) {
-                $result = $this->affectedRows();
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * @param array<string,mixed> $arrayBindKeyValue
-     *
-     * @query example: 'DELETE users SET name = :name , isActive = :isActive WHERE id = :id'
-     *
-     * @arrayBindKeyValue example [':id' => 32 ]
-     *
-     * @success return positive number affected rows and in error null
-     */
-    public function deleteQuery(string $deleteQuery, array $arrayBindKeyValue = []): int|null
-    {
-        $result = null;
-        if($this->isConnected()) {
-            if ($this->executeQuery($deleteQuery, $arrayBindKeyValue)) {
-                $result = $this->affectedRows();
-            }
-        }
-
-        return $result;
+        $connectionName ? $connectionName : DEFAULT_CONNECTION_NAME;
+        $conn = new PdoConnManager($connectionName);
+        parent::__construct($conn);
     }
 }
