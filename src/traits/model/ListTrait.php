61435541681141d4a2635d52fed0a963cd96b3cc
secure73 <secure73@gmail.com>
2023-11-26 19:17:40 +0100

    Add List Trait

---
+++ src/traits/model/ListTrait.php
@@ -0,0 +1,41 @@
+<?php
+namespace GemFramework\Traits\Model;
+use GemLibrary\Http\GemRequest;
+use GemLibrary\Http\JsonResponse;
+trait ListTrait
+{
+
+    public function list(GemRequest $request):JsonResponse
+    {
+        if(isset($request->post['page']))
+        {
+            $this->setPage((int)$request->post['page']);
+        }
+        if(isset($request->post['orderby']))
+        {
+            $this->setOrderBy($request->post['orderby']);
+        }
+        if(isset($request->post['find']))
+        {
+            $this->setFind($request->post['find']);
+        }
+        if(isset($request->post['between']))
+        {
+            $this->setBetween($request->post['between']);
+        }
+        if(isset($request->post['where']))
+        {
+            $this->setWhere($request->post['where']);
+        }
+        $jsonResponse = new JsonResponse();
+        if(!$this->getError())
+        {
+            $jsonResponse->success( $this->listQuery());
+        }
+        else
+        {
+            $jsonResponse->badRequest($this->getError());
+        }
+        return $jsonResponse;
+    }
+}
