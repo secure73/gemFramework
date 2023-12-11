d21d8b6653c8b319a89be3826c1d390ab23da333
secure73 <secure73@gmail.com>
2023-12-11 10:52:10 +0100

    Update DeleteTrait.php

--- src/traits/model/DeleteTrait.php
+++ src/traits/model/DeleteTrait.php
@@ -1,15 +1,26 @@
 <?php
 namespace GemFramework\Traits\Model;
 
-use GemFramework\Traits\Table\SafeDeleteQueryTrait;
+use GemFramework\Traits\Table\UpdateQueryTrait;
 use GemLibrary\Http\GemRequest;
 use GemLibrary\Http\JsonResponse;
 
 trait DeleteTrait
 {
-    use SafeDeleteQueryTrait;
+    use UpdateQueryTrait;
     public function delete(GemRequest $request):JsonResponse
     {
+        $jsonResponse = new JsonResponse();
+        if(!isset($request->post['id']))
+        {
+            $jsonResponse->badRequest('post id is not setted');
+            return $jsonResponse;
+        }
+        if(!is_numeric($request->post['id']) || $request->post['id'] < 1)
+        {
+            $jsonResponse->badRequest('id is not nummeric or less than 1');
+            return $jsonResponse;
+        }
         $jsonResponse = new JsonResponse();
         if(!$request->setPostToObject($this))
         {
