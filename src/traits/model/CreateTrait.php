3561eaed8eff16a6e0c2a2c35c99758a62d7cda8
secure73 <secure73@gmail.com>
2023-12-16 16:25:42 +0100

    Update CreateTrait.php

--- src/traits/model/CreateTrait.php
+++ src/traits/model/CreateTrait.php
@@ -1,25 +1,46 @@
 <?php
+
 namespace GemFramework\Traits\Model;
+
 use GemLibrary\Http\GemRequest;
 use GemLibrary\Http\JsonResponse;
 
 trait CreateTrait
 {
-    public function create(GemRequest $request):JsonResponse
+    public function create(GemRequest $request): JsonResponse
     {
         $jsonResponse = new JsonResponse();
-        if(!$request->setPostToObject($this))
-        {
+        if (!$request->setPostToObject($this)) {
             $jsonResponse->badRequest($request->getError());
             return $jsonResponse;
         }
-        $id = $this->insertSingleQuery();
-        if($id)
-        {
-            $jsonResponse->success($id,null,'created successfully');
+
+
+        $table = $this->setTable();
+        if (!$table) {
+            $jsonResponse->internalError('Table is not set in function setTable');
+            return $jsonResponse;
         }
-        else
-        {
+
+        $columns = '';
+        $params = '';
+        $arrayBind = [];
+        $query = "INSERT INTO {$table} ";
+        foreach ((object) $this as $key => $value) {
+            $columns .= $key . ',';
+            $params .= ':' . $key . ',';
+            $arrayBind[':' . $key] = $value;
+        }
+
+        $columns = rtrim($columns, ',');
+        $params = rtrim($params, ',');
+
+        $query .= " ({$columns}) VALUES ({$params})";
+        $id = $this->insertQuery($query, $arrayBind);
+
+        if ($id) {
+            $jsonResponse->success($id, null, 'created successfully');
+        } else {
             $jsonResponse->badRequest($this->getError());
         }
         return $jsonResponse;
