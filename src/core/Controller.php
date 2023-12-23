88141bdce48715fdf5fd43e8425a4af3394bcbfd
secure73 <secure73@gmail.com>
2023-12-23 13:11:18 +0100

    Update Controller.php

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -2,11 +2,24 @@
 namespace GemFramework\Core;
 use GemLibrary\Http\GemRequest;
 use GemLibrary\Http\JsonResponse;
+use GemFramework\Traits\Controller\IdTrait;
+use GemFramework\Traits\Controller\ListTrait;
+use GemFramework\Traits\Controller\RestoreTrait;
+use GemFramework\Traits\Controller\TrashTrait;
+use GemFramework\Traits\Controller\DeleteTrait;
+
 class Controller {
     protected JsonResponse $response;
     protected GemRequest $request;
-    public mixed $model;
-    public function __construct(GemRequest $request , mixed $model) {
+    public object $model;
+
+    use IdTrait;
+    use RestoreTrait;
+    use ListTrait;
+    use DeleteTrait;
+    use TrashTrait;
+
+    public function __construct(GemRequest $request , object $model) {
         $this->request = $request;
         $this->model = $model;
         $this->response = new JsonResponse();
