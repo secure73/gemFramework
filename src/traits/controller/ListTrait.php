73394a40a5e1363808473dd45b3adf4acc08dea8
secure73 <secure73@gmail.com>
2023-12-23 13:18:18 +0100

    Update ListTrait.php

--- src/traits/controller/ListTrait.php
+++ src/traits/controller/ListTrait.php
@@ -11,7 +11,7 @@ trait ListTrait{
 
     public function list():JsonResponse
     {
-        if(!$this->request->definePostSchema(['page' => 'int' , 'limit' => 'int'])){
+        if(!$this->request->definePostSchema(['?page' => 'int' , '?orderby' => 'string' , '?find' => 'string' , '?between' => 'string' , '?where' => 'string'])){
             $this->response->badRequest($this->request->error);
             return $this->response;
         }
