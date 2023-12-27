92d3ec0f7e597fc2b2d3392ed71e0d50a9c0a0d3
secure73 <secure73@gmail.com>
2023-12-27 12:30:57 +0000

    Add files via upload

---
+++ src/traits/model/ListObjectTrait.php
@@ -0,0 +1,39 @@
+<?php
+namespace GemFramework\Traits\Model;
+use GemLibrary\Http\GemRequest;
+trait ListObjectTrait
+{
+    /**
+     * @param GemRequest $request
+     * @return array<mixed>|false
+     */
+    public function listObjects(GemRequest $request):array|false
+    {
+        if(isset($request->post['page']))
+        {
+            $this->setPage((int)$request->post['page']);
+        }
+        if(isset($request->post['orderby']))
+        {
+            $this->setOrderBy($request->post['orderby']);
+        }
+        if(isset($request->post['find']))
+        {
+            $this->setFind($request->post['find']);
+        }
+        if(isset($request->post['between']))
+        {
+            $this->setBetween($request->post['between']);
+        }
+        if(isset($request->post['where']))
+        {
+            $this->setWhere($request->post['where']);
+        }
+        if(!$this->getError())
+        {
+            return $this->listQuery();
+        }
+        $this->error = $this->getError();
+        return false;
+    }
+}
