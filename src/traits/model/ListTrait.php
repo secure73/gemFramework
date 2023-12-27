d9fb298d785a7360928c2af5b945912e9fd6c325
secure73 <secure73@gmail.com>
2023-12-27 13:46:58 +0100

    Update ListTrait.php

--- src/traits/model/ListTrait.php
+++ src/traits/model/ListTrait.php
@@ -31,7 +31,7 @@ trait ListTrait
         if(!$this->getError())
         {
             $result = $this->listQuery();
-            $jsonResponse->success( $result,$result->count);
+            $jsonResponse->success( $result,$this->getCount());
         }
         else
         {
