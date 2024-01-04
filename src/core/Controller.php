24fb1910da9c6fd1e66e021cef4c466be6f8e3fc
secure73 <secure73@gmail.com>
2024-01-04 12:46:40 +0100

    Update Controller.php
    add function checkPosts to Controller

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -13,5 +13,18 @@ class Controller {
         $this->request = $request;
         $this->response = new JsonResponse();
     }
+
+    /**
+     * @param array<string> $postSchema
+     */
+    public function checkPosts(array $postSchema):bool
+    {
+        if(!$this->request->definePostSchema($postSchema))
+        {
+                $this->response->badRequest($this->request->error);
+                return false;
+        }
+        return true;
+    }
 }
 ?>
