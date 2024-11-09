6af80f108d076e8a9075b1b45a4a6f73b9e9ef92
Ali Khorsandfard <secure73@gmail.com>
2024-11-09 17:36:40 +0100

    Update Controller.php

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -3,6 +3,7 @@
 namespace Gemvc\Core;
 
 use Gemvc\Http\Request;
+use Gemvc\Http\Response;
 
 /**
  * @protected  GemLibrary\Http\Request $request
@@ -24,31 +25,36 @@ class Controller
 
     /**
      * @param array<string> $post_schema  Define Post Schema to validation
-     * @return bool
+     * @return void
      * validatePosts(['email'=>'email' , 'id'=>'int' , '?name' => 'string'])
+     * if not validated , automatic show response and die;
      * @help : ?name means it is optional
      */
-    protected function validatePosts(array $post_schema): bool
+    protected function validatePosts(array $post_schema): void
     {
         if (!$this->request->definePostSchema($post_schema)) {
-            $this->error = $this->request->error;
-            return false;
+            Response::badRequest($this->request->error)->show();
+            die;
         }
-        return true;
     }
 
     /**
      * Validates string lengths in a dictionary against min and max constraints.
-     *
-     * @param array<string> $post_schema A dictionary where keys are strings and values are strings in the format "key:min-value|max-value" (optional).
-     * @return bool True if all strings pass validation, False otherwise.
+     * if not validated , automatic show response and die;
+     * A dictionary where keys are strings and values are strings in the format "key:min-value|max-value" (optional).
+     * @param array<string> $post_schema an Array where keys are post name and values are strings in the format "key:min-value|max-value" (optional).
+     * validateStringPosts([
+     *     'username' => '3|15',  // Min length 3, max length 15
+     *     'password' => '8|',    // Min length 8, no max limit
+     *     'nickname' => '|20',   // No min limit, max length 20
+     *     'bio' => '',           // No min or max limit
+     * ]);
      */
-    protected function validateStringPosts(array $post_schema): bool
+    protected function validateStringPosts(array $post_string_schema): void
     {
-        if (!$this->request->validateStringPosts($post_schema)) {
-            $this->error = $this->request->error;
-            return false;
+        if (!$this->request->validateStringPosts($post_string_schema)) {
+            Response::badRequest($this->request->error)->show();
+            die;
         }
-        return true;
     }
 }
