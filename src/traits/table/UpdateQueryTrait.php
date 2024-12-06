a640b0b67fe4f4270d838a99e0b5f6f6900dec1f
Ali Khorsandfard <secure73@gmail.com>
2024-12-06 13:08:04 +0100

    Update UpdateQueryTrait.php

--- src/traits/table/UpdateQueryTrait.php
+++ src/traits/table/UpdateQueryTrait.php
@@ -1,5 +1,6 @@
 <?php
 namespace Gemvc\Traits\Table;
+use Gemvc\Http\Response;
 
 /**
  * @method updateSingleQuery()
@@ -9,20 +10,23 @@ namespace Gemvc\Traits\Table;
 trait UpdateQueryTrait
 {
     /**
-     * @update current instance
-     * @you can call affectedRows() and it shall be 1
+     * @return object<$this> updated Object or Show Internal Error and die
      * @error: $this->getError();
      */
-    public function updateSingleQuery(): int|null
+    public function updateSingleQuery(): object
     {
+        $result = false;
         $table = $this->getTable();
         if (!$table) {
             $this->setError('table is not set in function getTable');
-            return null;
+            Response::internalError($this->getError())->show();
+            die();
+
         }
         if (!isset($this->id) || $this->id < 1) {
-            $this->setError('property id does existed or not set in object');
-            return null;
+            $this->setError("property id does existed or not set in object for update Query $this->getTable()");
+            Response::internalError($this->getError())->show();
+            die();
         }
         $arrayBind = [];
         $table = $this->getTable();
@@ -36,47 +40,67 @@ trait UpdateQueryTrait
             $query = rtrim($query, ',');
             $query .= ' WHERE id = :id';
             $arrayBind[':id'] = $this->id;
-            return $this->updateQuery($query, $arrayBind);
+            $result = $this->updateQuery($query, $arrayBind);
         }
-        return null;
+        if($result === false) {
+            Response::internalError("error in update Query: $this->getTable(): ".$this->getError())->show();
+            die();
+        }
+        return $this;
     }
 
 
     /**
+     * @return int affected rows or Show Internal Error and die
      * @set a specific column to null based on condition whereColumn = $whereValue
      *
      * @exampel $this->setNull('deleted_at,'id',$this->id);
      *
      * @explain:  set deleted_at to null where id = $this->id
      */
-    public function setNullQuery(string $columnNameSetToNull, string $whereColumn, mixed $whereValue): int|null
+    public function setNullQuery(string $columnNameSetToNull, string $whereColumn, mixed $whereValue): int
     {
         $table = $this->getTable();
         if (!$table) {
             $this->setError('table is not set in function getTable');
-            return null;
+            Response::internalError($this->getError())->show();
+            die();
+
         }
         $query = "UPDATE {$table}  SET  {$columnNameSetToNull} = NULL  WHERE  {$whereColumn}  = :whereValue";
 
-        return $this->updateQuery($query, [':whereValue' => $whereValue]);
+        $result = $this->updateQuery($query, [':whereValue' => $whereValue]);
+        if($this->getError() || $result === false) {
+            Response::internalError("error in update Query: $this->getTable(): ".$this->getError())->show();
+            die();
+        }
+        return $result;
     }
 
     /**
+     * @return int affected rows or Show Internal Error and die
      * @set a specific column to time now based on condition whereColumn = $whereValue
      *
      * @exampel $order->setTimeNow('paid_at','id',$this->id);
      *
      * @explain:  set paid_at  to 18-08-2022 12:45:13 where id = $this->id
      */
-    public function setTimeNowQuery(string $columnNameSetToNowTomeStamp, string $whereColumn, mixed $whereValue): int|null
+    public function setTimeNowQuery(string $columnNameSetToNowTomeStamp, string $whereColumn, mixed $whereValue): int
     {
         $table = $this->getTable();
         if (!$table) {
             $this->setError('table is not set in function getTable');
-            return null;
+            Response::internalError($this->getError())->show();
+            die();
         }
-        $query = "UPDATE {$table}  SET  {$columnNameSetToNowTomeStamp} = NOW()  WHERE  {$whereColumn}  = :whereValue";
 
-        return $this->updateQuery($query, [':whereValue' => $whereValue]);
+
+        $query = "UPDATE {$table}  SET  {$columnNameSetToNowTomeStamp} = NOW()  WHERE  {$whereColumn}  = :whereValue";
+        $result = $this->updateQuery($query, [':whereValue' => $whereValue]);
+        if($this->getError() || $result === false) {
+            Response::internalError("error in update Query: $this->getTable(): ".$this->getError())->show();
+            die();
+        }
+        return $result;
     }
 }
