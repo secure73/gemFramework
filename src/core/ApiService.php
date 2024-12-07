aec879c8ab0629cdef64c5cf2cd87938bd578c1b
Ali Khorsandfard <secure73@gmail.com>
2024-12-07 20:22:50 +0100

    Update ApiService.php

--- src/core/ApiService.php
+++ src/core/ApiService.php
@@ -31,4 +31,39 @@ class ApiService
         $name = explode('\\', $name)[2];
         return Response::success("welcome to $name service");
     }
+
+        /**
+     * @param array<string> $post_schema  Define Post Schema to validation
+     * @return void
+     * validatePosts(['email'=>'email' , 'id'=>'int' , '?name' => 'string'])
+     * if not validated , automatic show response and die;
+     * @help : ?name means it is optional
+     */
+    protected function validatePosts(array $post_schema): void
+    {
+        if (!$this->request->definePostSchema($post_schema)) {
+            Response::badRequest($this->request->error)->show();
+            die;
+        }
+    }
+
+    /**
+     * Validates string lengths in a dictionary against min and max constraints.
+     * if not validated , automatic show response and die;
+     * A dictionary where keys are strings and values are strings in the format "key:min-value|max-value" (optional).
+     * @param array<string> $post_schema an Array where keys are post name and values are strings in the format "key:min-value|max-value" (optional).
+     * validateStringPosts([
+     *     'username' => '3|15',  // Min length 3, max length 15
+     *     'password' => '8|',    // Min length 8, no max limit
+     *     'nickname' => '|20',   // No min limit, max length 20
+     *     'bio' => '',           // No min or max limit
+     * ]);
+     */
+    protected function validateStringPosts(array $post_string_schema): void
+    {
+        if (!$this->request->validateStringPosts($post_string_schema)) {
+            Response::badRequest($this->request->error)->show();
+            die;
+        }
+    }
 }
