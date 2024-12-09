8f632790819439068881d57b3db75cc9f726b2f3
Ali Khorsandfard <secure73@gmail.com>
2024-12-09 20:59:23 +0100

    move base Queries to Table and delete unused Table Traits

--- src/core/Table.php
+++ src/core/Table.php
@@ -45,6 +45,42 @@ class Table extends PdoQuery
     }
 
     /**
+     * @return object<$this> created Object or Show Internal Error and die
+     * insert single object into  table
+     */
+    public final function insertSingle(): self
+    {
+        $table = $this->getTable();
+        if (!$table) {
+            Response::internalError($this->getError())->show();
+            die();
+        }
+
+        $columns = '';
+        $params = '';
+        $arrayBind = [];
+        $query = "INSERT INTO {$table} ";
+
+        foreach ((object) $this as $key => $value) {
+            $columns .= $key . ',';
+            $params .= ':' . $key . ',';
+            $arrayBind[':' . $key] = $value;
+        }
+
+        $columns = rtrim($columns, ',');
+        $params = rtrim($params, ',');
+
+        $query .= " ({$columns}) VALUES ({$params})";
+        $result = $this->insertQuery($query, $arrayBind);
+        if( $this->getError() || $result === false) {
+            Response::internalError("error in insert Query: $this->getTable(): ".$this->getError())->show();
+            die();
+        }
+        $this->id = $result;
+        return $this;
+    }
+
+    /**
     * @param array<mixed> $row
     */
     protected function fetchRow(array $row): void
