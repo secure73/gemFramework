4ee8bf3342086e281599c39dbc1fa3cdee156646
Ali Khorsandfard <secure73@gmail.com>
2025-02-14 22:06:42 +0100

    Update RemoveQuery.php

--- src/traits/table/RemoveQuery.php
+++ src/traits/table/RemoveQuery.php
@@ -1,13 +1,16 @@
 <?php
 namespace Gemvc\Traits\Table;
+use Gemvc\Http\JsonResponse;
 use Gemvc\Http\Response;
 
 /**
- * @method removeQuery()
- * @method removeConditionalQuery()
+ * @method JsonResponse remove():JsonResponse
+ * @method removeQuery(int $id):int
+ * @method removeConditionalQuery(string $whereColumn, mixed $whereValue, ?string $secondWhereColumn = null, mixed $secondWhereValue = null):int
  * NOTE !! This trait remove row from database
  */
-trait RemoveQuery {
+trait RemoveQuery
+{
     /**
      * @param int $id
      * @return int
@@ -17,6 +20,19 @@ trait RemoveQuery {
     public final function removeQuery(int $id): int
     {
         return $this->removeConditionalQuery('id', $id);
+    }
+
+    public function remove(): JsonResponse
+    {
+        if(!isset($this->id))
+        {
+            return Response::badRequest("id for this object is not set");
+        }
+        if(!$this->id || $this->id < 1){
+            return Response::badRequest("id shall be positive int");
+        }
+        $this->removeConditionalQuery("id", $this->id);
+        return Response::deleted($this, 1, "removed successfully");
     }
 
     /**
@@ -29,7 +45,7 @@ trait RemoveQuery {
         $table = $this->getTable();
         if (!$table) {
             Response::internalError('Table is not set in function getTable.')->show();
-           die();
+            die();
         }
 
         $query = "DELETE FROM {$table} WHERE {$whereColumn} = :{$whereColumn}";
@@ -37,13 +53,13 @@ trait RemoveQuery {
             $query .= " AND {$secondWhereColumn} = :{$secondWhereColumn}";
         }
 
-        $arrayBind = [':'.$whereColumn => $whereValue];
+        $arrayBind = [':' . $whereColumn => $whereValue];
         if ($secondWhereColumn) {
-            $arrayBind[':'.$secondWhereColumn] = $secondWhereValue;
+            $arrayBind[':' . $secondWhereColumn] = $secondWhereValue;
         }
         $result = $this->deleteQuery($query, $arrayBind);
-        if($result === false) {
-            Response::internalError("error in delete Query:". $this->getTable() .",".$this->getError())->show();
+        if ($result === false) {
+            Response::internalError("error in delete Query:" . $this->getTable() . "," . $this->getError())->show();
             die();
         }
         return $result;
