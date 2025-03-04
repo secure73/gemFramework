169a6302b87e55ebd71a4c99bb9a98ff5e3d0632
Ali Khorsandfard <secure73@gmail.com>
2025-03-04 23:00:37 +0100

    Update IdTrait.php

--- src/traits/model/IdTrait.php
+++ src/traits/model/IdTrait.php
@@ -1,20 +1,65 @@
 <?php
 namespace Gemvc\Traits\Model;
 
+use Gemvc\Http\JsonResponse;
+use Gemvc\Http\Response;
+
 /**
- * @method id(int $id)
- * @return self|null
+ * @method id(int $id):self|null
+ * @method byId(int $id):self
+ * @method findOrFail(int $id):self
+ * @method singleJson(int $id):JsonResponse
  */
 trait IdTrait
 {
+    /**
+     * @param int $id
+     * @return self|null
+     */
     public function id(int $id):self|null
     {
-        $found = $this->selectByIdQuery($id);
-        if(!$found)
+        $found = $this->select()->where('id', $id)->limit(1)->run();
+        if(count($found) == 0)
         {
-            $this->setError($this->getError());
             return null;
         }
-        return $this;
+        return $found[0];
+    }
+
+
+    /**
+     * @param int $id
+     * @return self|null
+     */
+    public function byId(int $id):self|null
+    {
+        return $this->id($id);
+    }
+
+    /**
+     * return self or show JsonResponse 404 error if not found
+     * @param int $id
+     * @return self
+     */
+    public function findOrFail(int $id): self
+    {
+        $result = $this->byId($id);
+        if(!$result)
+        {
+            Response::notFound('not found')->show();
+            die();
+        }
+        return $result;
+    }
+
+
+    /**
+     * return a single row as a json response, with success status and message or error status 404 and message
+     * @param int $id
+     * @return JsonResponse
+     */ 
+    public function singleJson(int $id): JsonResponse
+    {
+        return Response::success($this->byId($id),1,'success');
     }
 }
